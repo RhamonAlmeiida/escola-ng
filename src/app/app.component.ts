dac59e17d36dc79f958cc7c5950c1b19bea050b7
Rhamon Silva Almeida <75868@PROWAY.TREINA>
2025-05-12 18:23:24 -0300

    crud escola

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ButtonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'escola-ng';
+
+  constructor(private router: Router) { }
+
+  redirecionarCursos() {
+    // carregando/navegando para a rota /cursos
+    this.router.navigate(["/cursos"])
+  }
+
+  redirecionarHome(){
+    this.router.navigate(["/"])
+  }
+}
